2c11475814fdb595c14743b1fd3a3659936d759a
Karina Malij <karinamalij0303@gmail.com>
2026-04-14 21:43:35 +0300

    333

--- lab3/Header.h
+++ lab3/Header.h
@@ -1,24 +1,24 @@
 #pragma once
+
 #include <iostream>
-#include <fstream>
+#include <vector>
 #include <string>
 #include <map>
-#include <set>
-#include <iomanip>
-#include <vector>
+#include <fstream>
 #include <algorithm>
-#include <iostream>
 
 using namespace std;
+
+int extendedgcd(int a, int b, int& x, int& y);
+int modInverse(int a, int m);
+vector<int> LinCongr(int a, int b, int m);
 
 string cleanText(const string& text);
 string readFile(const string& filename);
 map<string, int> BigramFreq(const string& text);
 map<string, int> BigramFreqN(const string& text);
-void printTopBigrams(map<string, int> freq, ofstream& out, int top = 10);
-void printBigram(const string& text, ofstream& out);
-void printBigramN(const string& text, ofstream& out);
 
-int extendedgcd(int a, int b, int& x, int& y);
-int modInverse(int a, int m);
-vector<int> LinCongr(int a, int b, int m);
+int bigramToNum(const string& bg);
+string numToBigram(int x);
+string decrypt(const string& text, int a, int b);
+bool isRusText(const string& text);
